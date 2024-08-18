8ab1795beef427ff58be7c76999f1f8b61cec044
SamikshaDhadam <144090492+SamikshaDhadam@users.noreply.github.com>
2024-08-18 20:40:40 +0530

    Delete customer_spending.sql

--- customer_spending.sql
+++
@@ -1,9 +0,0 @@
-select
-  customer_id,
-  sum(amount) as total_spent,
-  count(transaction_id) as total_transactions,
-  max(transaction_date) as last_transaction_date
-from
-  {{ ref('transactions') }}
-group by
-  customer_id
