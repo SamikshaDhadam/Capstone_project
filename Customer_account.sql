895abefa0d8e0d48f166ddc337cf246211da9bcb
SamikshaDhadam <144090492+SamikshaDhadam@users.noreply.github.com>
2024-08-18 20:40:04 +0530

    Delete Customer_account.sql

--- Customer_account.sql
+++
@@ -1,9 +0,0 @@
-select
-  customer_id,
-  count(account_id) as total_accounts,
-  sum(current_balance) as total_balance,
-  max(open_date) as last_account_opened
-from
-  {{ ref('account') }}
-group by
-  customer_id
